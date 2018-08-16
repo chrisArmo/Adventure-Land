0e7e03b67934af3b042432925cf5a9e3f92fe783
Chris Armendarez <chris.michael.armo@gmail.com>
2018-08-15 23:47:11 -0700

    Add .eslintrc.json & gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+/* Dependencies */
+const gulp = require("gulp");
+
+/* Tasks */
+gulp.task("default", () => {
+    console.log("Hooray - you created a gulp task!");
+});
+
+gulp.task("html", () => {
+    console.log("Imagine something useful being done to your HTML here.");
+});

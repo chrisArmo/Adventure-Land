1ad2770ff8b79586b762d01fbee8cd7f33638a99
Chris Armendarez <chris.michael.armo@gmail.com>
2018-08-16 23:46:55 -0700

    Add postcss-import plugin to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,19 @@ watch = require("gulp-watch"),
 postcss = require("gulp-postcss"),
 autoprefixer = require("autoprefixer"),
 cssvars = require("postcss-simple-vars"),
-nested = require("postcss-nested");
+nested = require("postcss-nested"),
+cssImport = require("postcss-import");
 
 
 /* Tasks */
+gulp.task("styles", () => {
+    return gulp.src("./app/assets/styles/styles.css")
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest("./app/temp/styles"));
+});
 
+gulp.task("watch", () => {
+    watch("./app/assets/styles/**/*.css", () => {
+        gulp.start("styles");
+    });
+});
